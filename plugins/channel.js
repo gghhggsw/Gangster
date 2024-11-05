75e5d15249f5d5b3a92ae6460f95956f9b8fef5d
TCRONEB HACKX <thelastcroneb@gmail.com>
2024-11-05 17:48:31 +0200

    Add files via upload

---
+++ plugins/channel.js
@@ -0,0 +1,14 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _𝙸𝙱𝙱𝙾𝚃-V10-MD_
+│ 📝 *Description:* _I'm 𝙸𝙱𝙱𝙾𝚃-𝚇𝙼𝙳-𝚅10. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
+│ 👤 *Owner:* _𝚃𝚌𝚛𝚘𝚗𝚎𝚋 𝙷𝚊𝚌𝚔𝚡_
+│ 🌐 *𝙶𝚛𝚘𝚞𝚙:* https://chat.whatsapp.com/LqD6utmLF699JIW9R82O95
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['xchannel', 'channel' ] 
+
+export default handler
