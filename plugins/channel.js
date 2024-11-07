237e0bde1721cf2bc7f13264aa4e9c59d4e8ca66
TCRONEB HACKX <thelastcroneb@gmail.com>
2024-11-07 11:47:14 +0200

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,8 +1,8 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _𝙸𝙱𝙱𝙾𝚃-V10-MD_
-│ 📝 *Description:* _I'm 𝙸𝙱𝙱𝙾𝚃-𝚇𝙼𝙳-𝚅10. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
+╭⭑⭑⭑★✪ *GANGSTER* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _GANGSTER-V10-XMD_
+│ 📝 *Description:* _I'm GANGSTER 𝙸𝙱𝙱𝙾𝚃-𝚇𝙼𝙳-𝚅10. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
 │ 👤 *Owner:* _𝚃𝚌𝚛𝚘𝚗𝚎𝚋 𝙷𝚊𝚌𝚔𝚡_
 │ 🌐 *𝙶𝚛𝚘𝚞𝚙:* https://chat.whatsapp.com/LqD6utmLF699JIW9R82O95
 ╰━━━━━━━━━━━━━━━━╯
