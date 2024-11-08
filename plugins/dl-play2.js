cb68a453177c37d70b88f926a4ea1ba48fb6eff6
TCRONEB HACKX <thelastcroneb@gmail.com>
2024-11-09 01:51:53 +0200

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,135 +1,85 @@
-import fetch from 'node-fetch';
-import ytSearch from 'yt-search';
 import ytdl from 'ytdl-core';
+import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import os from 'os';
 import axios from 'axios';
 
-let handler = async (m, { command, conn, text }) => {
+const streamPipeline = promisify(pipeline);
+
+let handler = async (message, { conn, command, text, usedPrefix }) => {
   if (!text) {
-    throw "No text to search, please enter the name of the song you want to play.\n\n*EXAMPLE:\n#play2  - Hope xxxtentacion*";
+    throw "Use example " + usedPrefix + command + " <query>";
   }
+  
+  await message.react('⏳');
   try {
-    if (command === "play2") {
-      conn.reply(m.chat, "*_sending your audio..._*", m);
-      try {
-        let response = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`);
-        let result = await response.json();
-        let sent = await conn.sendMessage(m.chat, {
-          'audio': { 'url': result.result.audio },
-          'fileName': "error.mp3",
-          'mimetype': "audio/mp4"
-        }, { 'quoted': m });
-        if (!sent) {
-          await conn.sendFile(m.chat, result.result.audio, "error.mp3", null, m, false, { 'mimetype': "audio/mp4" });
-        }
-      } catch {
-        let ytResult = await ytPlay(text);
-        let audioUrl = ytResult.result2[0].audio || ytResult.result2[1].audio || ytResult.result2[2].audio || ytResult.result2;
-        await conn.sendMessage(m.chat, {
-          'audio': { 'url': audioUrl },
-          'fileName': "error.mp3",
-          'mimetype': "audio/mp4"
-        }, { 'quoted': m });
-      }
+    const query = encodeURIComponent(text);
+    const searchResponse = await axios.get(`https://weeb-api.vercel.app/ytsearch?query=${query}`);
+    const video = searchResponse.data.results[0];
+
+    if (!video) {
+      throw "Video not found, try another title.";
     }
 
-    if (command === "playvid") {
-      conn.reply(m.chat, "*_⏳Processing your video...⏳_*", m);
-      try {
-        let ytVideoResult = await ytPlayVid(text);
-        await conn.sendMessage(m.chat, {
-          'video': { 'url': ytVideoResult.result },
-          'fileName': 'error.mp4',
-          'caption': "_XLICON V2 MD_",
-          'thumbnail': ytVideoResult.thumb,
-          'mimetype': "video/mp4"
-        }, { 'quoted': m });
-      } catch {
-        let response = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`);
-        let result = await response.json();
-        await conn.sendFile(m.chat, result.result.video, "error.mp4", "_XLICON V2 MD_", m);
+    const { title, thumbnail, duration, views, uploaded, url } = video;
+
+    const videoInfo = `⬡▸  ••๑⋯ ⬡▸  Y O U T U B E ⬡▸  ⋯⋅๑•• ✼\n\n  ⬡▸  Title: ${title}\n\n  ⬡▸  Duration: ${duration}\n\n  ⬡▸  Views: ${views}\n\n  ⬡▸  Upload: ${uploaded}\n\n  ⬡▸  Link: ${url}\n\n⊱─━━━━⊱༻XLICON●v2༺⊰━━━━─⬡▸`;
+
+    conn.sendMessage(message.chat, {
+      image: { url: thumbnail },
+      caption: videoInfo,
+      footer: "Author"
+    }, { quoted: message });
+
+    const audioStream = ytdl(url, {
+      filter: "audioonly",
+      quality: "highestaudio"
+    });
+
+    const tmpDir = os.tmpdir();
+    const audioPath = `${tmpDir}/${title}.mp3`;
+    const audioWriteStream = fs.createWriteStream(audioPath);
+
+    await streamPipeline(audioStream, audioWriteStream);
+
+    const audioMessage = {
+      audio: { url: audioPath },
+      mimetype: "audio/mpeg",
+      ptt: false,
+      fileName: `${title}.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: url,
+          title: title,
+          body: "Here is your song made by XLICON-v2",
+          sourceUrl: url,
+          thumbnail: await (await conn.getFile(thumbnail)).data
+        }
       }
-    }
+    };
+
+    await conn.sendMessage(message.chat, audioMessage, { quoted: message });
+
+    fs.unlink(audioPath, (err) => {
+      if (err) {
+        console.error("Failed to delete audio file:", err);
+      } else {
+        console.log("Deleted audio file:", audioPath);
+      }
+    });
+
   } catch (error) {
-    m.reply("*Error occurred, please try again later.*");
+    console.error(error);
+    throw "An error occurred while searching for YouTube videos or fetching the audio.";
   }
 };
 
-handler.help = ["play2", "playvid"].map(cmd => cmd + " <text>");
-handler.tags = ["downloader"];
-handler.command = ["play2", 'playvid'];
+handler.help = ["play1"].map(command => command + " <query>");
+handler.tags = ['downloader'];
+handler.command = /^play1$/i;
+handler.exp = 0;
+
 export default handler;
-
-function bytesToSize(bytes) {
-  return new Promise((resolve, reject) => {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-    resolve((bytes / 1024 ** i).toFixed(1) + " " + sizes[i]);
-  });
-}
-
-async function ytMp3(url) {
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(url).then(async info => {
-      let audioFormats = info.formats.filter(format => format.mimeType === "audio/webm; codecs=\"opus\"");
-      let formatted = [];
-      for (let format of audioFormats) {
-        let size = await bytesToSize(format.contentLength);
-        formatted.push({ 'audio': format.url, 'size': size });
-      }
-      let valid = formatted.filter(item => item.audio !== undefined && item.size !== undefined);
-      let shortUrl = await axios.get(`https://tinyurl.com/api-create.php?url=${valid[0].audio}`);
-      resolve({
-        'title': info.videoDetails.title,
-        'result': shortUrl.data,
-        'result2': valid,
-        'thumb': info.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
-      });
-    }).catch(reject);
-  });
-}
-
-async function ytMp4(url) {
-  return new Promise(async (resolve, reject) => {
-    ytdl.getInfo(url).then(async info => {
-      let videoFormats = info.formats.filter(format => format.container === "mp4" && format.hasVideo === true && format.hasAudio === true);
-      let formatted = [];
-      for (let format of videoFormats) {
-        let size = await bytesToSize(format.contentLength);
-        formatted.push({ 'video': format.url, 'quality': format.qualityLabel, 'size': size });
-      }
-      let valid = formatted.filter(item => item.video !== undefined && item.size !== undefined && item.quality !== undefined);
-      let shortUrl = await axios.get(`https://tinyurl.com/api-create.php?url=${valid[0].video}`);
-      resolve({
-        'title': info.videoDetails.title,
-        'result': shortUrl.data,
-        'result2': valid[0].video,
-        'thumb': info.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
-      });
-    }).catch(reject);
-  });
-}
-
-async function ytPlay(query) {
-  return new Promise((resolve, reject) => {
-    ytSearch(query).then(async result => {
-      let videos = result.videos.slice(0, 5);
-      let urls = videos.map(video => video.url);
-      let firstUrl = urls[0];
-      let mp3Result = await ytMp3(firstUrl);
-      resolve(mp3Result);
-    }).catch(reject);
-  });
-}
-
-async function ytPlayVid(query) {
-  return new Promise((resolve, reject) => {
-    ytSearch(query).then(async result => {
-      let videos = result.videos.slice(0, 5);
-      let urls = videos.map(video => video.url);
-      let firstUrl = urls[0];
-      let mp4Result = await ytMp4(firstUrl);
-      resolve(mp4Result);
-    }).catch(reject);
-  });
-}
